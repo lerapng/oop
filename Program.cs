51e982ee6f9ae806e499f936e22b9e861a16ce6c
lerapng <103666862+lerapng@users.noreply.github.com>
2025-10-01 14:31:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Task1
+{
+    public class Program
+    {
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static string GetMessage(int number)
+        {
+            if (IsEven(number))
+            {
+                return "Двері відкриваються!";
+            }
+            else
+            {
+                return "Двері зачинені...";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Введіть число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            string result = GetMessage(number);
+            Console.WriteLine(result);
+        }
+    }
+}
