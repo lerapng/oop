0b6ae0858e00dd581db33b02db5f098f75f88224
lerapng <103666862+lerapng@users.noreply.github.com>
2025-10-01 14:31:56 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,56 @@
-﻿using System.Text;
-
-namespace Task1
+﻿namespace Task2
 {
     public class Program
     {
-        public static bool IsEven(int number)
+        public static int[] GenerateRandomArray(int size, int min, int max)
         {
-            return number % 2 == 0;
+            Random rand = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(min, max + 1);
+            }
+            return array;
         }
 
-        public static string GetMessage(int number)
+        public static int GetSum(int[] numbers)
         {
-            if (IsEven(number))
+            int sum = 0;
+            foreach (int num in numbers)
             {
-                return "Двері відкриваються!";
+                sum += num;
             }
-            else
-            {
-                return "Двері зачинені...";
-            }
+            return sum;
         }
 
+        public static double GetAverage(int[] numbers)
+        {
+            return (double)GetSum(numbers) / numbers.Length;
+        }
+
+        public static int GetMin(int[] numbers)
+        {
+            int min = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num < min) min = num;
+            }
+            return min;
+        }
+
+        public static int GetMax(int[] numbers)
+        {
+            int max = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num > max) max = num;
+            }
+            return max;
+
+        }
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
 
-            Console.Write("Введіть число: ");
-            int number = int.Parse(Console.ReadLine());
-
-            string result = GetMessage(number);
-            Console.WriteLine(result);
         }
     }
 }
